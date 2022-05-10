a5b3a40dc3fda63c98bbeecd953f24107cbf3fc4
Daman <daman.bhatia46@gmail.com>
2022-05-10 12:56:54 +0530

    final commit 1.0

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  
+  messageForm: FormGroup | any; // chk & validate for a group of form controls
+  submitted = false;  //flag for successful form submission
+  success = false;   // flag for sucessful form validation
+  
+  constructor(private formBuilder: FormBuilder) {
+    
+   }
+
+  ngOnInit(): void {
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
+ 
+    if (this.messageForm.invalid) {
+        return;
+    }
+ 
+    this.success = true;
+}
+
+}
